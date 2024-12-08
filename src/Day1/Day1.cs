ae4582bbb8820c212f9522a744170b672490381c
Jeroen Broekhuizen <j_w_broekhuizen@hotmail.com>
2024-12-09 00:32:41 +0100

    first day complete

--- src/Day1/Day1.cs
+++ src/Day1/Day1.cs
@@ -1,6 +1,61 @@
-﻿namespace advent_of_code_2024.Day1
+﻿namespace advent_of_code_2024.Day1;
+
+public static class Day1
 {
-    public class Day1
+    public static int ProcessPart1(ReadOnlySpan<char> input)
     {
+        var (left, right) = GetLists(input);
+
+        var orderedLeft = left.Order();
+        var orderedRight = right.Order();
+        var itemCount = orderedLeft.Count();
+        var output = 0;
+        for (var i = 0; i < itemCount; i++)
+        {
+            output += Math.Abs(orderedLeft.ElementAt(i) - orderedRight.ElementAt(i));
+        }
+
+        return output;
+    }
+
+    public static int ProcessPart2(ReadOnlySpan<char> input)
+    {
+        var (left, right) = GetLists(input);
+
+        var itemCount = left.Count();
+        var output = 0;
+        for (var i = 0; i < itemCount; i++)
+        {
+            var number = left.ElementAt(i);
+            output += number * right.Count(c => c == number);
+        }
+
+        return output;
+    }
+
+    public static (List<int> left, List<int> right) GetLists(ReadOnlySpan<char> input)
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+
+        var split = input.Split('\n');
+        foreach (var range in split)
+        {
+            var line = input[range];
+
+            var lineIndex = 0;
+            while (line[lineIndex] != ' ')
+            {
+                lineIndex++;
+            }
+            left.Add(int.Parse(line[0..lineIndex]));
+            while (line[lineIndex] == ' ')
+            {
+                lineIndex++;
+            }
+            right.Add(int.Parse(line[lineIndex..]));
+        }
+
+        return (left, right);
     }
 }
--- test/Day1Tests.cs
+++ test/Day1Tests.cs
@@ -1,5 +1,54 @@
-﻿namespace test;
+﻿using advent_of_code_2024.Day1;
+using FluentAssertions;
+
+namespace test;
 
 public class Day1Tests
 {
+
+    [Fact]
+    public void Example1()
+    {
+        Day1.ProcessPart1(
+"""
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""".AsSpan())
+            .Should()
+            .Be(11);
+    }
+
+    [Fact]
+    public void Input1()
+    {
+        Day1.ProcessPart1(File.ReadAllText("Day1/input.txt"))
+            .Should().Be(3508942);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        Day1.ProcessPart2(
+"""
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""".AsSpan())
+            .Should()
+            .Be(31);
+    }
+
+    [Fact]
+    public void Input2()
+    {
+        Day1.ProcessPart2(File.ReadAllText("Day1/input.txt"))
+            .Should().Be(26593248);
+    }
 }
